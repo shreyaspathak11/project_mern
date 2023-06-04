29ff6ee6853ea5e2354a298de597d8ace9151ef0
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-05 00:01:03 +0530

    this also done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
-import { RiSearch2Line } from 'react-icons/ri';
+import {FaSearch} from 'react-icons/fa' ;
 // Main section component
 const MainSection = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
 
-  const handleHover = () => {
-    setIsExpanded(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsExpanded(false);
-  };
     return (
+      <>
       <section className="main-section">
         <div className="description">
-          <h1>Welcome to Mindful Musings</h1>
-          <p>Share and explore inspiring quotes and thoughts.</p>
-        </div>
+          <h1>Dive into a world of profound wisdom</h1>
+          </div>
+            <div className="description">
+          <p>Share and explore inspiring quotes and thoughts.
         
-        <div className={`search-bar ${isExpanded ? 'expanded' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
-      <input type="text" placeholder="Just Tell me what You need!" />
-      <button>
-        <RiSearch2Line />
-      </button>
-    </div>
-
-      </section>
+          </p>
+        </div>
+       
+        <div class="search-container">
+  <input class="search" type="search" placeholder="Find it here" />        
+  <button className="button"><FaSearch/></button>
+</div>
+ </section>
+        
+</>
+      
     );
   };
   
