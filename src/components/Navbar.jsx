79e4873bd8144d05b4abff21cd806e7ffb631354
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-08 03:20:05 +0530

    gi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,34 +7,49 @@ import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 
 // Navbar component
 const Navbar = () => {
-  const [user, setUser] = useState(null);
-  const [profile, setProfile] = useState(null);
+ 
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const login = useGoogleLogin({
-    onSuccess: (codeResponse) => setUser(codeResponse),
-    onError: (error) => console.log('Login Failed:', error)
-  });
+  const [user, setUser] = useState();
+
+
+  const googleAuth = ({ profileObj }) => {
+    axios({
+      method: "post",
+      url: "/auth/google/signup",
+      data: {
+        googleId: profileObj.googleId,
+        email: profileObj.email,
+       
+        name: profileObj.name,
+      },
+    })
+      .then((res) => {
+
+        console.log(res.data)
+      })
+      .catch((err) => console.log(err));
+  };
+
+
 
   useEffect(() => {
-    if (user) {
-      axios
-        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-            Accept: 'application/json'
-          }
-        })
-        .then((res) => {
-          setProfile(res.data);
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [user]);
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get("/profile");
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    fetchProfile();
+  }, []);
 
+  
   const logOut = () => {
     googleLogout();
-    setProfile(null);
+    setUser(null);
     setShowDropdown(false);
   };
 
@@ -52,26 +67,31 @@ const Navbar = () => {
       </div>
 
       {/* Login with Google */}
-      {profile ? (
+      {user ? (
         <div className="profile-container">
           <img
-            src={profile.picture}
-            alt={profile.name}
+            src={user.picture}
+            alt={user.name}
             className="profile-image"
             onClick={toggleDropdown}
           />
           {showDropdown && (
             <ul className="dropdown-menu">
-              <li className="dropdown-item">{profile.name}</li>
+              <li className="dropdown-item">{user.name}</li>
               <li className="dropdown-item" onClick={logOut}>Logout</li>
               <li className="dropdown-item">Profile</li>
             </ul>
           )}
         </div>
       ) : (
-        <button className="button-profile" onClick={() => login()}>
-          Sign in
-        </button>
+        <useGoogleLogin
+      clientId="678252387684-lrt3j7dvlqjs81g8sa107qerdp5pked6.apps.googleusercontent.com"
+      onSuccess={googleAuth}
+      onFailure={googleAuth}
+      cookiePolicy={"single_host_origin"}
+    >
+      <span>Sign Up with Google</span>
+    </useGoogleLogin>
       )}
     </nav>
   );
@@ -79,3 +99,8 @@ const Navbar = () => {
 
 
 export default Navbar;
+
+
+
+
+
