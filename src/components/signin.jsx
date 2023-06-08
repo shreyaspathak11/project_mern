fc70d0163625a6bcb65ec076b7bf0ab4f9512e3e
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-06-08 15:33:24 +0530

    changesss 0000

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,7 +1,46 @@
-import React, { useState } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 
 function SignIn() {
+    const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+            Accept: 'application/json'
+          }
+        })
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+    setShowDropdown(false);
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+
+
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
